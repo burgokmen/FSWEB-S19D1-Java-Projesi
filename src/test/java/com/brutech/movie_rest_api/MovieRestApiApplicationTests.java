d50092701e740d0201e269a75fff247a05cf3574
burgokmen <burgokmen@gmail.com>
2023-10-30 14:54:28 +0300

    init

---
+++ src/test/java/com/brutech/movie_rest_api/MovieRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brutech.movie_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
